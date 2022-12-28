105c21fdb8c74d7fcf59fe4eb586fd2bcc4ccc38
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-12-28 12:03:55 +0300

    Zenject scene context setup

---
+++ Assets/_Game/Scripts/DI/GameInstaller.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using Zenject;
+
+namespace Trivia
+{
+    public class GameInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+
+        }
+    }
+}
