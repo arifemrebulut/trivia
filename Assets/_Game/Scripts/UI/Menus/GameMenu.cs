ad08782e5b53027d0c6386eac181c5d577a6f089
Emre Bulut <arifemrebulut@gmail.com>
2022-12-30 12:20:33 +0300

    Score animation created

--- Assets/_Game/Scripts/UI/Menus/GameMenu.cs
+++ Assets/_Game/Scripts/UI/Menus/GameMenu.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,6 +15,7 @@ namespace Trivia
         [SerializeField] private Button mainMenuButton;
         [SerializeField] private Button resumeButton;
 
+        private int previousScore;
         private int totalScore;
 
         private void OnEnable()
@@ -49,13 +51,17 @@ namespace Trivia
 
         private void EarnScore(int earnAmount)
         {
+            previousScore = totalScore;
+
             totalScore += earnAmount;
 
-            scoreTMP.text = totalScore.ToString();
+            StartCoroutine(AnimateScoreText());
         }
 
         private void LostScore(int lostAmount)
         {
+            previousScore = totalScore;
+
             if (totalScore - lostAmount < 0)
             {
                 totalScore = 0;
@@ -65,7 +71,33 @@ namespace Trivia
                 totalScore -= lostAmount;
             }
 
-            scoreTMP.text = totalScore.ToString();
+            StartCoroutine(AnimateScoreText());
+        }
+
+        private IEnumerator AnimateScoreText()
+        {
+            if (totalScore > previousScore)
+            {
+                while (previousScore + 1 < totalScore)
+                {
+                    yield return new WaitForSeconds(1f / 10f);
+
+                    previousScore++;
+
+                    scoreTMP.text = previousScore.ToString();
+                }
+            }
+            else
+            {
+                while (previousScore - 1 > totalScore)
+                {
+                    yield return new WaitForSeconds(1f / 10f);
+
+                    previousScore--;
+
+                    scoreTMP.text = previousScore.ToString();
+                }
+            }
         }
     }
 }
